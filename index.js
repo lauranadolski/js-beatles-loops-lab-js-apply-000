128d173c1c1a33e12c9bdbd9f4d27969fce39860
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-09 00:42:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+// Beatles Loops Lab
+// Laura Nadolski
+// 4/8/18
+
+// theBeatlesPlay() accepts two arrays as parameters: one of
+// musicians and one of instruments. It produces a new array
+// filled with strings that are each a combination of the various
+// musicians and instruments in the parameter-arrays.
+function theBeatlesPlay(musicians, instruments) {
+ var newarray = [];
+ for (var i = 0; i < musicians.length; i++) {
+   newarray.push(`${musicians[i]} plays ${instruments[i]}`);
+ }
+ return newarray; 
+}
+
+// johnLennonFacts() accepts an array of facts and loops over
+// said array, adding "!!!" to each one.
+function johnLennonFacts(facts) {
+  var i = 0;
+  var factsarray = [];
+  while (i < facts.length) {
+    factsarray.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return factsarray;
+}
+
+// iLoveTheBeatles accepts a number as a parameter, and if
+function iLoveTheBeatles(number) {
+  var lovearray = [];
+  var i = 0;
+  do {
+    lovearray.push(`I love the Beatles!`);
+  }
+  while (i < number);
+  return lovearray;
+}
